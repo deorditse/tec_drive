1c3c37c7429762d166f3d594c1740c06ed39c4ed
Dmitry <94318108+deorditse@users.noreply.github.com>
2026-01-05 16:27:56 +0300

    Update .DS_Store

--- dist/assets/EducationPage-Bdt0YDbW.js
+++
@@ -1,1 +0,0 @@
-import{r as t,a as e,j as o,c}from"./index-Bez5gse0.js";const n=t.memo(a=>{const{className:s}=a,{t:r}=e();return o.jsx("div",{className:c("",{},[s]),children:"EducationPage"})});n.displayName="EducationPage";export{n as default};
